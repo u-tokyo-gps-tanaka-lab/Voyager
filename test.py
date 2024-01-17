9e7e4cdf14c8cdee0d1b3ba55248dec2b4dedb42
tappy27 <38070396+tappy27@users.noreply.github.com>
2024-01-17 17:17:13 +0900

    Update test.py
    Fix a test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from voyager import Voyager
 
 # You can also use mc_port instead of azure_login, but azure_login is highly recommended
 
-openai_api_key = "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"
+openai_api_key = "your_key"
 
 voyager = Voyager(
     mc_port=49488,
